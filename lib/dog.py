0f6bf155a4338c7a7118453ce72a351d94618b0f
Maggie-Karuri <karurimargaret111@gmail.com>
2024-05-31 02:45:28 +0300

    attributes

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,30 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="", breed=""):
+        self.set_name(name)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            self._name = None
+            print("Name must be string between 1 and 25 characters.")
+    name = property(get_name, set_name)
+    
+    def _init_(self, breed=""):
+         self.set_breed(breed)
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            self._breed = None
+            print(f"Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
